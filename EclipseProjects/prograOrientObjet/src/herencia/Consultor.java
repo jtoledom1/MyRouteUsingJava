f62a48ffe7f48fbf0f8be5ff338afd2d12f025a3
jtoledom1 <jtoledom1@outlook.com>
2025-05-27 17:13:38 -0600

    Polimorfismo

--- EclipseProjects/prograOrientObjet/src/herencia/Consultor.java
+++ EclipseProjects/prograOrientObjet/src/herencia/Consultor.java
@@ -1,5 +1,36 @@
 package herencia;
 
-public class Consultor {
+public class Consultor extends Persona{
+	String nombre_Consultora;
+	int num_consultor;
+	
+	public Consultor() {
+
+	}
+
+	public Consultor(int id, String dni, String nombre, String apellido, String domicilio, String telefono,
+			String nombre_Consultora, int num_consultor) {
+		super(id, dni, nombre, apellido, domicilio, telefono);
+		this.nombre_Consultora = nombre_Consultora;
+		this.num_consultor = num_consultor;
+	}
+
+	public String getNombre_Consultora() {
+		return nombre_Consultora;
+	}
+
+	public void setNombre_Consultora(String nombre_Consultora) {
+		this.nombre_Consultora = nombre_Consultora;
+	}
+
+	public int getNum_consultor() {
+		return num_consultor;
+	}
+
+	public void setNum_consultor(int num_consultor) {
+		this.num_consultor = num_consultor;
+	}
+	
+	
 
 }
--- EclipseProjects/prograOrientObjet/src/herencia/Herencia.java
+++ EclipseProjects/prograOrientObjet/src/herencia/Herencia.java
@@ -4,6 +4,27 @@ public class Herencia {
 
 	public static void main(String[] args) {
 		
+		Empleado emple1 = new Empleado();
+		
+		emple1.getNum_legajo();
+		emple1.getNombre();
+		
+		Consultor consul = new Consultor();
+		
+		consul.getNombre_Consultora();
+		consul.getApellido();
+		
+		Persona vector [] = new Persona[5];
+		vector [0] = new Persona();
+		vector [0] = new Empleado();
+		vector [0] = new Consultor();
+		
+		Persona perso = new Persona();
+		Consultor consu = new Consultor();
+		
+		perso = consu;
+		// consu = perso; 		/*Estaa línea daría error porque el superclass es Persona*/
+		
 	}
 
 }
